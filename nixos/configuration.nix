f05339962be8aad12766298bb395cf96c1414f75
RobbingDaHood <16130319+RobbingDaHood@users.noreply.github.com>
2024-04-04 21:23:02 +0200

    added cataclysm game

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -152,6 +152,7 @@
         # https://nixos.wiki/wiki/Fish
         fish # hos to be defined here because setting it as default shell requires root access: https://www.reddit.com/r/NixOS/comments/z16mt8/comment/ix9r4b2/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
         steam-run # Configurations for the steam client.
+        cataclysm-dda
   ];
 
 
