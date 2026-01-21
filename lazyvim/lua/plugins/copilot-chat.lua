0d8ccf18fcc03b901fad21966e06111c26c14708
Daniel Petersen <dapeters@akamai.com>
2026-01-21 08:58:06 +0100

    lazyvim: Added custom prompts to copilot and configured some smaller stuff.

--- lazyvim/lua/plugins/copilot-chat.lua
+++ lazyvim/lua/plugins/copilot-chat.lua
@@ -3,14 +3,31 @@ return {
   opts = {
     -- Use copilot built-in selection logic
     selection = "#selection",
+    auto_insert_mode = false, -- That results in navigating suddenly writing.
     window = {
-      width = 0.2,
+      width = 0.3,
       -- This is currently being ignored, putting it here in case it gets fixed at some point.
       position = "left",
     },
+    prompts = {
+      ReviewAllChanges = {
+        name = "Summarize Git Diff and Suggest Improvements",
+        description = "Summarizes changes from 'git diff development...' and suggests improvements.",
+        prompt = [[
+        @copilot git diff development...
+
+    Analyze the latest changes between the current branch and 'development'.
+    Write a short summary of the changes and list possible improvements.
+    Do not include the raw diff in your response.
+    Tell me if there are added some TODOs in the diff. 
+    ]],
+      },
+      -- Add more custom prompts here as needed
+    },
   },
   config = function(_, opts)
-    require("CopilotChat").setup(opts)
+    local CopilotChat = require("CopilotChat")
+    CopilotChat.setup(opts)
 
     -- Unet <C-l> for copilot chat, because that would reset the window.
     -- Delay unmapping to after CopilotChat sets its keymaps
